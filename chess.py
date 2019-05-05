aebc1b8af1a8e5d506a7a2ed8156e0deb19652ec
Brian Dumbacher <brian.dumbacher@gmail.com>
2019-05-05 18:04:24 -0400

    Update chess.py
    Write code to print board

--- chess.py
+++ chess.py
@@ -1,118 +1,36 @@
 #Name:     chess.py
 #Purpose:  Execute chess moves
 
-def isPuzzleSolved(puzzle):
-    return puzzle == [["1","2","3"],["4","5","6"],["7","8"," "]]
-
-def slidesValid(puzzle):
-    slides = []
-    iBlank = 0
-    jBlank = 0
-    for i in [0,1,2]:
-        for j in [0,1,2]:
-            if puzzle[i][j] == " ":
-                iBlank = i
-                jBlank = j
-    for i in [n for n in [iBlank-1,iBlank+1] if n in [0,1,2]]:
-        slides.append(puzzle[i][jBlank])
-    for j in [n for n in [jBlank-1,jBlank+1] if n in [0,1,2]]:
-        slides.append(puzzle[iBlank][j])
-    return slides
-
-def randomPuzzle():
-    puzzle = [["1","2","3"],["4","5","6"],["7","8"," "]]
-    numSlides = 0
-    while isPuzzleSolved(puzzle) or numSlides < 50:
-        random.seed()
-        slides = slidesValid(puzzle)
-        slide = slides[random.randint(0, len(slides) - 1)]
-        puzzle = updatePuzzle(puzzle, slide)
-        numSlides = numSlides + 1
-    return puzzle
-
-def printPuzzle(puzzle):
+def printBoard(board):
     print("")
-    print("@@@@@@@@@@@@@@@@@@@")
-    print("@     @     @     @")
-    print("@  " + puzzle[0][0] + "  @  " + puzzle[0][1] + "  @  " + puzzle[0][2] + "  @")
-    print("@     @     @     @")
-    print("@@@@@@@@@@@@@@@@@@@")
-    print("@     @     @     @")
-    print("@  " + puzzle[1][0] + "  @  " + puzzle[1][1] + "  @  " + puzzle[1][2] + "  @")
-    print("@     @     @     @")
-    print("@@@@@@@@@@@@@@@@@@@")
-    print("@     @     @     @")
-    print("@  " + puzzle[2][0] + "  @  " + puzzle[2][1] + "  @  " + puzzle[2][2] + "  @")
-    print("@     @     @     @")
-    print("@@@@@@@@@@@@@@@@@@@")
+    for i in [8,7,6,5,4,3,2,1]:
+        for j in [1,2,3,4,5,6,7,8]:
+            square = " "
+            if len(board[i][j]) == 2:
+                if board[i][j][0] == "w":
+                    square = board[i][j][1]
+                if board[i][j][0] == "b":
+                    square = "\033[94m" + board[i][j][1] + "\033[0m"
+            print(square, end="")
+        print("")
     print("")
-    return
-
-def invalidSlide(puzzle, slide):
-    return slide not in slidesValid(puzzle)
-
-def updatePuzzle(puzzle, slide):
-    iBlank = 0
-    jBlank = 0
-    iSlide = 0
-    jSlide = 0
-    for i in [0,1,2]:
-        for j in [0,1,2]:
-            if puzzle[i][j] == " ":
-                iBlank = i
-                jBlank = j
-            elif puzzle[i][j] == slide:
-                iSlide = i
-                jSlide = j
-    puzzle[iBlank][jBlank] = slide
-    puzzle[iSlide][jSlide] = " "
-    return puzzle
-
-def printEndPuzzle(numSlides):
-    print("==================================================")
-    print("You solved the puzzle in " + str(numSlides) + " slides.")
-    print("Congratulations!")
-    print("==================================================")
-    print("")
-    return
-
-def playPuzzle(puzzleStart):
-    
-    #Parameters
-    solveFlag = False
-    puzzle = puzzleStart
-    numSlides = 0
-    
-    #Slide loop
-    while solveFlag == False:
-        printPuzzle(puzzle)
-        slide = ""
-        while invalidSlide(puzzle, slide):
-            slide = input("Slide: ")
-        numSlides = numSlides + 1
-        puzzle = updatePuzzle(puzzle, slide)
-        solveFlag = isPuzzleSolved(puzzle)
-    
-    printPuzzle(puzzle)
-    printEndPuzzle(numSlides)
     return
 
 def main():
     
     #Global parameters
-    loopFlag = True
+    row1 = ["wR","wN","wB","wQ","wK","wB","wN","wR"]
+    row2 = ["wP","wP","wP","wP","wP","wP","wP","wP"]
+    row3 = ["","","","","","","",""]
+    row4 = ["","","","","","","",""]
+    row5 = ["","","","","","","",""]
+    row6 = ["","","","","","","",""]
+    row7 = ["bP","bP","bP","bP","bP","bP","bP","bP"]
+    row8 = ["bR","bN","bB","bQ","bK","bB","bN","bR"]
+    board = [row1, row2, row3, row4, row5, row6, row7, row8]
     
-    #Game loop
-    while loopFlag:
-        puzzleStart = randomPuzzle()
-        playPuzzle(puzzleStart)
-        newPuzzle = ""
-        while newPuzzle not in ["n", "y"]:
-            newPuzzle = input("Another puzzle? Y/N: ")
-            newPuzzle = newPuzzle.lower()
-        if newPuzzle == "n":
-            loopFlag = False
-    print("")
+    printBoard(board)
+
     return
 
 if __name__ == "__main__":
