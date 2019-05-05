0bd14a103a7c0533c28cfe475a62532e6aa2aeb7
Brian Dumbacher <brian.dumbacher@gmail.com>
2019-05-05 18:11:27 -0400

    Update chess.py
    Fix errors

--- chess.py
+++ chess.py
@@ -3,8 +3,24 @@
 
 def printBoard(board):
     print("")
-    for i in [8,7,6,5,4,3,2,1]:
-        for j in [1,2,3,4,5,6,7,8]:
+    for i in [7,6,5,4,3,2,1,0]:
+        if i == 0:
+            print(" A | ", end="")
+        elif i == 1:
+            print(" B | ", end="")
+        elif i == 2:
+            print(" C | ", end="")
+        elif i == 3:
+            print(" D | ", end="")
+        elif i == 4:
+            print(" E | ", end="")
+        elif i == 5:
+            print(" F | ", end="")
+        elif i == 6:
+            print(" G | ", end="")
+        elif i == 7:
+            print(" H | ", end="")
+        for j in [0,1,2,3,4,5,6,7]:
             square = " "
             if len(board[i][j]) == 2:
                 if board[i][j][0] == "w":
@@ -13,6 +29,8 @@ def printBoard(board):
                     square = "\033[94m" + board[i][j][1] + "\033[0m"
             print(square, end="")
         print("")
+    print("     --------")
+    print("     12345678")
     print("")
     return
 
@@ -20,12 +38,12 @@ def main():
     
     #Global parameters
     row1 = ["wR","wN","wB","wQ","wK","wB","wN","wR"]
-    row2 = ["wP","wP","wP","wP","wP","wP","wP","wP"]
+    row2 = ["wp","wp","wp","wp","wp","wp","wp","wp"]
     row3 = ["","","","","","","",""]
     row4 = ["","","","","","","",""]
     row5 = ["","","","","","","",""]
     row6 = ["","","","","","","",""]
-    row7 = ["bP","bP","bP","bP","bP","bP","bP","bP"]
+    row7 = ["bp","bp","bp","bp","bp","bp","bp","bp"]
     row8 = ["bR","bN","bB","bQ","bK","bB","bN","bR"]
     board = [row1, row2, row3, row4, row5, row6, row7, row8]
     
