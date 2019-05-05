013903d5ee01f0a4bc0705785bddd773bb9c7b8b
Brian Dumbacher <brian.dumbacher@gmail.com>
2019-05-05 16:23:05 -0400

    Update sliding_puzzle.py
    Fix random puzzle generator

--- sliding_puzzle.py
+++ sliding_puzzle.py
@@ -3,34 +3,8 @@
 
 import random
 
-def randomPuzzle():
-    nums = ["1","2","3","4","5","6","7","8"," "]
-    mix = ["1","2","3","4","5","6","7","8"," "]
-    while mix == nums:
-        random.seed()
-        random.shuffle(mix)
-    return [mix[0:3],mix[3:6],mix[6:9]]
-
-def printPuzzle(puzzle):
-    print("")
-    print("@@@@@@@@@@@@@")
-    print("@   @   @   @")
-    print("@ " + puzzle[0][0] + " @ " + puzzle[0][1] + " @ " + puzzle[0][2] + " @")
-    print("@   @   @   @")
-    print("@@@@@@@@@@@@@")
-    print("@   @   @   @")
-    print("@ " + puzzle[1][0] + " @ " + puzzle[1][1] + " @ " + puzzle[1][2] + " @")
-    print("@   @   @   @")
-    print("@@@@@@@@@@@@@")
-    print("@   @   @   @")
-    print("@ " + puzzle[2][0] + " @ " + puzzle[2][1] + " @ " + puzzle[2][2] + " @")
-    print("@   @   @   @")
-    print("@@@@@@@@@@@@@")
-    print("")
-    return
-
-def invalidSlide(puzzle, slide):
-    slidesValid = []
+def slidesValid(puzzle):
+    slides = []
     iBlank = 0
     jBlank = 0
     for i in [0,1,2]:
@@ -39,10 +13,44 @@ def invalidSlide(puzzle, slide):
                 iBlank = i
                 jBlank = j
     for i in [n for n in [iBlank-1,iBlank+1] if n in [0,1,2]]:
-        slidesValid.append(puzzle[i][jBlank])
+        slides.append(puzzle[i][jBlank])
     for j in [n for n in [jBlank-1,jBlank+1] if n in [0,1,2]]:
-        slidesValid.append(puzzle[iBlank][j])
-    return slide not in slidesValid
+        slides.append(puzzle[iBlank][j])
+    return slides
+
+def randomPuzzle():
+    startPuzzle = [["1","2","3"],["4","5","6"],["7","8"," "]]
+    mixPuzzle = [["1","2","3"],["4","5","6"],["7","8"," "]]
+    numSlides = 0
+    while mixPuzzle == startPuzzle or numSlides < 20:
+        random.seed()
+        slides = slidesValid(mixPuzzle)
+        slide = slides[random.ranint(0, len(slides) - 1)]
+        mixPuzzle = updatePuzzle(mixPuzzle, slide)
+        num = num + 1
+        
+    return mixPuzzle
+
+def printPuzzle(puzzle):
+    print("")
+    print("@@@@@@@@@@@@@@@@@@@")
+    print("@     @     @     @")
+    print("@  " + puzzle[0][0] + "  @  " + puzzle[0][1] + "  @  " + puzzle[0][2] + "  @")
+    print("@     @     @     @")
+    print("@@@@@@@@@@@@@@@@@@@")
+    print("@     @     @     @")
+    print("@  " + puzzle[1][0] + "  @  " + puzzle[1][1] + "  @  " + puzzle[1][2] + "  @")
+    print("@     @     @     @")
+    print("@@@@@@@@@@@@@@@@@@@")
+    print("@     @     @     @")
+    print("@  " + puzzle[2][0] + "  @  " + puzzle[2][1] + "  @  " + puzzle[2][2] + "  @")
+    print("@     @     @     @")
+    print("@@@@@@@@@@@@@@@@@@@")
+    print("")
+    return
+
+def invalidSlide(puzzle, slide):
+    return slide not in slidesValid(puzzle)
 
 def updatePuzzle(puzzle, slide):
     iBlank = 0
